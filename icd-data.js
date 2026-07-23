900af03927ffdb8237cea465a9cfef62afe8808c
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-23 17:21:52 +0200

    Add all ICD-10 2019 chapters in Albanian

--- icd-data.js
+++ icd-data.js
@@ -1,7 +1,32 @@
 window.MEDINDEX_ICD10 = {
-  version: 'WHO ICD-10 2019',
+  version: 'WHO ICD-10 Versioni 2019',
   language: 'sq',
+  browserUrl: 'https://icd.who.int/browse10/2019/en',
   sourceUrl: 'https://icd.who.int/browse10/2019/en#/J85-J86',
+  chapters: [
+    { number: 'I', range: 'A00–B99', whoPath: 'A00-B99', title: 'Disa sëmundje infektive dhe parazitare', englishTitle: 'Certain infectious and parasitic diseases', keywords: ['infeksion', 'infektive', 'parazitare'] },
+    { number: 'II', range: 'C00–D48', whoPath: 'C00-D48', title: 'Neoplazitë', englishTitle: 'Neoplasms', keywords: ['tumor', 'kancer', 'neoplazi'] },
+    { number: 'III', range: 'D50–D89', whoPath: 'D50-D89', title: 'Sëmundjet e gjakut dhe të organeve gjakformuese dhe disa çrregullime që përfshijnë mekanizmin imunitar', englishTitle: 'Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism', keywords: ['gjak', 'hematologji', 'imunitar', 'anemi'] },
+    { number: 'IV', range: 'E00–E90', whoPath: 'E00-E90', title: 'Sëmundjet endokrine, ushqimore dhe metabolike', englishTitle: 'Endocrine, nutritional and metabolic diseases', keywords: ['endokrine', 'ushqimore', 'metabolike', 'diabet', 'tiroide'] },
+    { number: 'V', range: 'F00–F99', whoPath: 'F00-F99', title: 'Çrregullimet mendore dhe të sjelljes', englishTitle: 'Mental and behavioural disorders', keywords: ['mendore', 'psikiatri', 'sjellje', 'depresion', 'ankth'] },
+    { number: 'VI', range: 'G00–G99', whoPath: 'G00-G99', title: 'Sëmundjet e sistemit nervor', englishTitle: 'Diseases of the nervous system', keywords: ['neurologji', 'nervor', 'tru', 'nerva'] },
+    { number: 'VII', range: 'H00–H59', whoPath: 'H00-H59', title: 'Sëmundjet e syrit dhe të adnekseve të tij', englishTitle: 'Diseases of the eye and adnexa', keywords: ['syri', 'oftalmologji', 'shikim'] },
+    { number: 'VIII', range: 'H60–H95', whoPath: 'H60-H95', title: 'Sëmundjet e veshit dhe të procesit mastoid', englishTitle: 'Diseases of the ear and mastoid process', keywords: ['veshi', 'otorinolaringologji', 'dëgjim', 'mastoid'] },
+    { number: 'IX', range: 'I00–I99', whoPath: 'I00-I99', title: 'Sëmundjet e sistemit të qarkullimit të gjakut', englishTitle: 'Diseases of the circulatory system', keywords: ['kardiologji', 'zemër', 'enë gjaku', 'qarkullim'] },
+    { number: 'X', range: 'J00–J99', whoPath: 'J00-J99', title: 'Sëmundjet e sistemit respirator', englishTitle: 'Diseases of the respiratory system', keywords: ['respirator', 'frymëmarrje', 'mushkëri', 'pulmonologji'] },
+    { number: 'XI', range: 'K00–K93', whoPath: 'K00-K93', title: 'Sëmundjet e sistemit tretës', englishTitle: 'Diseases of the digestive system', keywords: ['gastroenterologji', 'tretje', 'stomak', 'zorrë', 'mëlçi'] },
+    { number: 'XII', range: 'L00–L99', whoPath: 'L00-L99', title: 'Sëmundjet e lëkurës dhe të indit nënlëkuror', englishTitle: 'Diseases of the skin and subcutaneous tissue', keywords: ['lëkurë', 'dermatologji', 'ind nënlëkuror'] },
+    { number: 'XIII', range: 'M00–M99', whoPath: 'M00-M99', title: 'Sëmundjet e sistemit muskuloskeletal dhe të indit lidhor', englishTitle: 'Diseases of the musculoskeletal system and connective tissue', keywords: ['muskuloskeletal', 'kocka', 'nyje', 'muskuj', 'reumatologji'] },
+    { number: 'XIV', range: 'N00–N99', whoPath: 'N00-N99', title: 'Sëmundjet e sistemit gjenitourinar', englishTitle: 'Diseases of the genitourinary system', keywords: ['veshka', 'urinare', 'urologji', 'gjinekologji'] },
+    { number: 'XV', range: 'O00–O99', whoPath: 'O00-O99', title: 'Shtatzënia, lindja dhe periudha e lehonisë', englishTitle: 'Pregnancy, childbirth and the puerperium', keywords: ['shtatzëni', 'lindje', 'lehoni', 'obstetrikë'] },
+    { number: 'XVI', range: 'P00–P96', whoPath: 'P00-P96', title: 'Disa gjendje që e kanë origjinën në periudhën perinatale', englishTitle: 'Certain conditions originating in the perinatal period', keywords: ['perinatale', 'i porsalindur', 'neonatologji'] },
+    { number: 'XVII', range: 'Q00–Q99', whoPath: 'Q00-Q99', title: 'Keqformimet kongjenitale, deformimet dhe anomalitë kromozomale', englishTitle: 'Congenital malformations, deformations and chromosomal abnormalities', keywords: ['kongjenitale', 'keqformime', 'deformime', 'kromozomale', 'gjenetike'] },
+    { number: 'XVIII', range: 'R00–R99', whoPath: 'R00-R99', title: 'Simptomat, shenjat dhe gjetjet jonormale klinike dhe laboratorike, të paklasifikuara diku tjetër', englishTitle: 'Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified', keywords: ['simptoma', 'shenja', 'gjetje klinike', 'gjetje laboratorike'] },
+    { number: 'XIX', range: 'S00–T98', whoPath: 'S00-T98', title: 'Lëndimet, helmimet dhe disa pasoja të tjera të shkaqeve të jashtme', englishTitle: 'Injury, poisoning and certain other consequences of external causes', keywords: ['lëndime', 'trauma', 'helmime', 'dëmtime'] },
+    { number: 'XX', range: 'V01–Y98', whoPath: 'V01-Y98', title: 'Shkaqet e jashtme të sëmundshmërisë dhe vdekshmërisë', englishTitle: 'External causes of morbidity and mortality', keywords: ['shkaqe të jashtme', 'aksident', 'sëmundshmëri', 'vdekshmëri'] },
+    { number: 'XXI', range: 'Z00–Z99', whoPath: 'Z00-Z99', title: 'Faktorët që ndikojnë në gjendjen shëndetësore dhe kontaktin me shërbimet shëndetësore', englishTitle: 'Factors influencing health status and contact with health services', keywords: ['status shëndetësor', 'shërbime shëndetësore', 'kontroll', 'kontakt'] },
+    { number: 'XXII', range: 'U00–U99', whoPath: 'U00-U99', title: 'Kodet për qëllime të veçanta', englishTitle: 'Codes for special purposes', keywords: ['kode speciale', 'qëllime të veçanta'] }
+  ],
   chapter: {
     code: 'J00–J99',
     title: 'Sëmundjet e sistemit respirator'
