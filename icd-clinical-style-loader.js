58c6254d276d8f6ba2ea2389f40c57095ae75b86
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-07-26 17:37:32 +0200

    Load refined ICD cards after the TailAdmin shell

--- icd-clinical-style-loader.js
+++ icd-clinical-style-loader.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const ID = 'icdClinicalWorkspaceStyles';
-  const HREF = '/icd-clinical-workspace.css?v=20260725-1';
+  const HREF = '/icd-tailadmin-cards-v2.css?v=20260726-1';
 
   function ensure() {
     let link = document.getElementById(ID);
@@ -10,9 +10,9 @@
       link = document.createElement('link');
       link.id = ID;
       link.rel = 'stylesheet';
-      link.href = HREF;
       link.dataset.icdClinicalWorkspace = '1';
     }
+    if (link.getAttribute('href') !== HREF) link.href = HREF;
     if (document.head.lastElementChild !== link) document.head.appendChild(link);
   }
 
