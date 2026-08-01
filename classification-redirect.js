ca99c805a616dd29846d7679efeafe4746c63382
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-01 22:52:25 +0200

    Redirect legacy classification page to registry table

---
+++ classification-redirect.js
@@ -0,0 +1,21 @@
+(() => {
+  'use strict';
+
+  const target = new URL('/index.html', location.origin);
+  const incoming = new URLSearchParams(location.search);
+  incoming.forEach((value, key) => target.searchParams.set(key, value));
+
+  const legacyHash = decodeURIComponent(location.hash.slice(1) || '')
+    .trim()
+    .toUpperCase()
+    .replace(/\s+/g, '');
+
+  if (/^[A-Z]\d{2}/.test(legacyHash)) {
+    target.searchParams.set('atc', legacyHash.slice(0, 3));
+  } else if (/^[A-Z]$/.test(legacyHash)) {
+    target.searchParams.set('atc', legacyHash);
+  }
+
+  target.searchParams.delete('classification');
+  location.replace(`${target.pathname}${target.search}`);
+})();
