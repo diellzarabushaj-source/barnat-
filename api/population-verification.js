e8ae9781154887d97f1c903620c024db073a406b
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-01 15:09:25 +0200

    Mbaj një slot rezervë të funksioneve Vercel

--- api/population-verification.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const PopulationVerification = require('../lib/population-verification.js');
-
-module.exports = async function handler(req, res) {
-  return PopulationVerification.handle(req, res);
-};
