bab944265e8f6b94a68689c7e27a45e4566f143b
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-01 15:10:09 +0200

    Bashko verifikimin e popullatës me editorin ekzistues

--- api/clinical-editor.js
+++ api/clinical-editor.js
@@ -1,7 +1,18 @@
 'use strict';
 
 const ClinicalEditor = require('../lib/clinical-editor.js');
+const PopulationVerification = require('../lib/population-verification.js');
+
+function populationVerificationRequest(req) {
+  if (req.query?.populationVerification !== undefined) return true;
+  try {
+    return new URL(String(req.url || ''), 'https://medindex.local').searchParams.has('populationVerification');
+  } catch {
+    return false;
+  }
+}
 
 module.exports = async function handler(req, res) {
+  if (populationVerificationRequest(req)) return PopulationVerification.handle(req, res);
   return ClinicalEditor.handle(req, res);
 };
