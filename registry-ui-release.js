d1fba69c8c67b3f77d3ee46cc4be6aa5595bbdd4
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-01 13:23:47 +0200

    Bump final registry UI release

--- registry-ui-release.js
+++ registry-ui-release.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = 'registry-ui-20260801-7';
+  const RELEASE = 'registry-ui-20260801-8';
   const STORAGE_KEY = 'medindex.registry.ui.release';
   const SESSION_KEY = 'medindex.registry.ui.cache-cleared';
 
