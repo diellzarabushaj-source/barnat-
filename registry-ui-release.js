981c7c5e40956d1ae3073c51b7a69bcdb28bcd7d
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-05 17:31:39 +0200

    chore: align registry UI release token

--- registry-ui-release.js
+++ registry-ui-release.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = 'registry-ui-20260805-15';
+  const RELEASE = 'registry-ui-20260805-19';
   const STORAGE_KEY = 'medindex.registry.ui.release';
   const SESSION_KEY = 'medindex.registry.ui.cache-cleared';
 
