954dcc8e837818d524eba2ad21e17fecd86b663e
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-09 16:47:25 +0200

    Route verified dose safety catalog

--- api/dosage.js
+++ api/dosage.js
@@ -2,25 +2,40 @@
 
 const dosageHandler = require('../lib/dosage-handler.js');
 const doseCalculatorHandler = require('../lib/dose-calculator-handler.js');
+const doseSafetyHandler = require('../lib/dose-safety-handler.js');
 
-function isCalculatorRequest(req) {
+function requestView(req) {
   try {
     const url = new URL(req?.url || '/api/dosage', 'http://medindex.local');
-    return url.searchParams.get('view') === 'calculator';
+    return url.searchParams.get('view') || '';
   } catch {
-    return false;
+    return '';
   }
+}
+
+function isCalculatorRequest(req) {
+  return requestView(req) === 'calculator';
+}
+
+function isSafetyRequest(req) {
+  return requestView(req) === 'safety';
 }
 
 async function handler(req, res) {
   if (isCalculatorRequest(req)) return doseCalculatorHandler(req, res);
+  if (isSafetyRequest(req)) return doseSafetyHandler(req, res);
   return dosageHandler(req, res);
 }
 
 Object.assign(handler, dosageHandler);
 handler.getDoseCalculatorCatalog = doseCalculatorHandler.getCatalog;
 handler.buildDoseCalculatorCatalog = doseCalculatorHandler.buildCatalog;
+handler.getDoseSafetyCatalog = doseSafetyHandler.getCatalog;
+handler.buildDoseSafetyCatalog = doseSafetyHandler.buildCatalog;
 handler._doseCalculatorTest = doseCalculatorHandler._test;
+handler._doseSafetyTest = doseSafetyHandler._test;
 handler.isCalculatorRequest = isCalculatorRequest;
+handler.isSafetyRequest = isSafetyRequest;
+handler.requestView = requestView;
 
 module.exports = handler;
