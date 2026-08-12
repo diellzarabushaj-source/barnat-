345dbd21c292231e85524c4c4141b5f2e041a8c1
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-12 09:31:28 +0200

    Route Phase 4 targeted clinical cards through dosage API

--- api/dosage.js
+++ api/dosage.js
@@ -3,6 +3,7 @@
 const dosageHandler = require('../lib/dosage-handler.js');
 const doseCalculatorHandler = require('../lib/dose-calculator-handler.js');
 const doseSafetyHandler = require('../lib/dose-safety-handler.js');
+const dosageCardHandler = require('../lib/dosage-card-handler.js');
 
 function requestView(req) {
   try {
@@ -21,9 +22,14 @@ function isSafetyRequest(req) {
   return requestView(req) === 'safety';
 }
 
+function isCardRequest(req) {
+  return requestView(req) === 'card';
+}
+
 async function handler(req, res) {
   if (isCalculatorRequest(req)) return doseCalculatorHandler(req, res);
   if (isSafetyRequest(req)) return doseSafetyHandler(req, res);
+  if (isCardRequest(req)) return dosageCardHandler(req, res);
   return dosageHandler(req, res);
 }
 
@@ -34,8 +40,10 @@ handler.getDoseSafetyCatalog = doseSafetyHandler.getCatalog;
 handler.buildDoseSafetyCatalog = doseSafetyHandler.buildCatalog;
 handler._doseCalculatorTest = doseCalculatorHandler._test;
 handler._doseSafetyTest = doseSafetyHandler._test;
+handler._dosageCardTest = dosageCardHandler._test;
 handler.isCalculatorRequest = isCalculatorRequest;
 handler.isSafetyRequest = isSafetyRequest;
+handler.isCardRequest = isCardRequest;
 handler.requestView = requestView;
 
 module.exports = handler;
