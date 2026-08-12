6ac561f05a6e173cb8e765d260b4572f694a2151
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-12 10:46:46 +0200

    Route approved population through dosage API

--- api/dosage.js
+++ api/dosage.js
@@ -4,6 +4,7 @@ const dosageHandler = require('../lib/dosage-handler.js');
 const doseCalculatorHandler = require('../lib/dose-calculator-handler.js');
 const doseSafetyHandler = require('../lib/dose-safety-handler.js');
 const dosageCardHandler = require('../lib/dosage-card-handler.js');
+const approvedPopulationHandler = require('../lib/approved-population-handler.js');
 
 function requestView(req) {
   try {
@@ -30,10 +31,15 @@ function isCardsRequest(req) {
   return requestView(req) === 'cards';
 }
 
+function isApprovedPopulationRequest(req) {
+  return requestView(req) === 'approved-population';
+}
+
 async function handler(req, res) {
   if (isCalculatorRequest(req)) return doseCalculatorHandler(req, res);
   if (isSafetyRequest(req)) return doseSafetyHandler(req, res);
   if (isCardRequest(req) || isCardsRequest(req)) return dosageCardHandler(req, res);
+  if (isApprovedPopulationRequest(req)) return approvedPopulationHandler(req, res);
   return dosageHandler(req, res);
 }
 
@@ -42,6 +48,8 @@ handler.getDoseCalculatorCatalog = doseCalculatorHandler.getCatalog;
 handler.buildDoseCalculatorCatalog = doseCalculatorHandler.buildCatalog;
 handler.getDoseSafetyCatalog = doseSafetyHandler.getCatalog;
 handler.buildDoseSafetyCatalog = doseSafetyHandler.buildCatalog;
+handler.getApprovedPopulationItems = approvedPopulationHandler.getApprovedPopulationItems;
+handler.getPediatricOnlyRegistryNumbers = approvedPopulationHandler.getPediatricOnlyRegistryNumbers;
 handler._doseCalculatorTest = doseCalculatorHandler._test;
 handler._doseSafetyTest = doseSafetyHandler._test;
 handler._dosageCardTest = dosageCardHandler._test;
@@ -49,6 +57,7 @@ handler.isCalculatorRequest = isCalculatorRequest;
 handler.isSafetyRequest = isSafetyRequest;
 handler.isCardRequest = isCardRequest;
 handler.isCardsRequest = isCardsRequest;
+handler.isApprovedPopulationRequest = isApprovedPopulationRequest;
 handler.requestView = requestView;
 
 module.exports = handler;
