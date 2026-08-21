4cc8f630dd502280ff51a0089164ccaf573f229a
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-21 22:58:24 +0200

    Restore original WebGL noise interpolation

--- login-v2-canvas.js
+++ login-v2-canvas.js
@@ -59,7 +59,8 @@
         vec2 u = f * f * (3.0 - 2.0 * f);
         return mix(
           mix(hash(i),                hash(i + vec2(1.0, 0.0)), u.x),
-          mix(hash(i + vec2(0.0,1.0)), hash(i + vec2(1.0, 1.0)), u.y)
+          mix(hash(i + vec2(0.0,1.0)), hash(i + vec2(1.0, 1.0)), u.x),
+          u.y
         );
       }
 
