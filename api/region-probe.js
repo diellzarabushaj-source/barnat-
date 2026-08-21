eb932e331a37de294cca317f835f6af244bef2cb
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-08-21 17:16:51 +0200

    Remove temporary function region probe

--- api/region-probe.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({ region:process.env.VERCEL_REGION || 'unknown' });
-};
