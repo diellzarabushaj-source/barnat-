87c76b5785355781f41f2ff7d7d05ee880e3b31a
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-09-12 23:13:05 +0200

    Antibiotikët: include hardening audits in full repo test suite

--- tests/antibiotics-daily-dose-test.js
+++ tests/antibiotics-daily-dose-test.js
@@ -5,3 +5,6 @@ require('./antibiotics-phase4-prescription-test.js');
 require('./antibiotics-phase5-hospital-test.js');
 require('./antibiotics-phase6-preparation-test.js');
 require('./antibiotics-phase7-calculator-cases-test.js');
+require('./antibiotics-clinical-hardening-contract-test.js');
+require('./antibiotics-clinical-source-pins-test.js');
+require('./antibiotics-ui-safety-contract-test.js');
